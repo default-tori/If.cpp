6f3a601ef995d233714e03bf3b917b998aedbddf
Tori <hikikitori7@gmail.com>
2026-03-12 15:52:51 +0100

    Create if.cpp

---
+++ if.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int number;
+cout << "enter a number" << endl;
+cin >> number;
+if (number >= 0){
+cout << "this number is positive" << endl;
+}
+}
